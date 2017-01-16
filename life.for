21dfb37da7ff8af8cc7ac094b4ea3cd988ba4229
Energy0124 <energystar.0124@gmail.com>
2017-01-17 00:36:05 +0800

    read and print file header

--- life.for
+++ life.for
@@ -15,10 +15,24 @@ c     Student ID : 1155062557
 c     Email Addr : alanalan0124@yahoo.com.hk
       program life
 c       force explicit type declarations
-        IMPLICIT NONE
+        implicit none
 c       variable declaration
-        CHARACTER*256 arg
+        character arg*80
+        character name*80
+        integer file, ios, gen, row, col
         call getarg(1, arg)
         write(*,*) arg
-        stop
+        file=1
+        open(unit=file, iostat= ios, file=arg, status='old')
+        if ( ios .neqv. 0 ) stop 'error opening file '
+        read(file, *, iostat= ios) name
+        if ( ios .neqv. 0 ) stop 'error reading name '
+        read(file, *, iostat= ios) gen
+        if ( ios .neqv. 0 ) stop 'error reading gen '
+        read(file, *, iostat= ios) row, col
+        if ( ios .neqv. 0 ) stop 'error reading row and col '
+        write(*,*) name
+        write(*,*) gen
+        write(*,*) row, col
+        stop 'quit normally'
       end
