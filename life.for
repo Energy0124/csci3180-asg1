3b103109c2f816408106b093bd8571335f9eb685
Energy0124 <energystar.0124@gmail.com>
2017-01-18 03:45:13 +0800

    complete cell counting

--- life.for
+++ life.for
@@ -16,21 +16,81 @@ c     Email Addr : alanalan0124@yahoo.com.hk
 c
 c     Language:
 c     fortran 77
+c
+c     function
+c     check if two patter are the same
       logical function issame(s1,s2,row,col)
         character s1(1:100)*80, s2(1:100)*80
         integer row, col
-        integer ir, ic
+        integer ir
         ir=1
-        ic=1
         issame=.true.
  300    if ( ir.gt.row ) goto 310
           write(*, *) "start comparing line ", ir
           if(s1(ir).ne.s2(ir)) issame=.false.
           write(*, *) "end comparing line ", ir
-          ic=1
           ir=ir+1
         goto 300
  310    write(*, *) "done comparing lines"
+        return
+      end
+      subroutine copypa(pat1,pat2,row,col)
+        character pat1(1:100)*80, pat2(1:100)*80
+        integer row, col
+        integer ir, ic
+        ir=1
+        write(*, *) "start copying pattern"
+ 501    if ( ir.gt.row ) goto 520
+          pat2(ir)=pat1(ir)
+          ir=ir+1
+        goto 501
+ 520    write(*, *) "end copying pattern"
+        return
+      end
+      integer function countc(pat1,pat2,row,col,crow,ccol)
+        character pat1(1:100)*80, pat2(1:100)*80
+        integer row, col,crow,ccol
+        integer ir, ic, jr, jc
+        ir=-1
+        ic=-1
+        countc=0
+ 600    if ( ir.gt.1 ) goto 610
+          write(*, *) "start counting line ", ir
+ 601      if ( ic.gt.1 ) goto 611
+            if(crow+ir.lt.1 .or. crow+ir.gt.row)goto 620
+            if(ccol+ic.lt.1 .or. ccol+ic.gt.col)goto 620
+            if(ir.eq.0.and.ic.eq.0)goto 620
+            if(pat1(crow+ir)(ccol+ic:ccol+ic).eq.'*') countc=countc+1
+ 620        ic=ic+1
+          goto 601
+ 611      write(*, *) "end counting line ", ir
+          ic=-1
+          ir=ir+1
+        goto 600
+ 610    write(*, *) "done counting row ", crow, " col ", ccol,
+     +    " ,count: ", countc
+        return
+      end
+c     subroutine
+c     simulate pattern
+      subroutine sim(pat1,pat2,row,col)
+        character pat1(1:100)*80, pat2(1:100)*80
+        integer row, col , countc, celsum
+        integer ir, ic, jr, jc
+        ir=1
+        ic=1
+ 400    if ( ir.gt.row ) goto 410
+          write(*, *) "start simulating line ", ir
+ 401      if ( ic.gt.col ) goto 411
+            celsum=0
+            celsum=countc(pat1,pat2,row,col,ir,ic)
+            ic=ic+1
+          goto 401
+ 411      write(*, *) "end simulating line ", ir
+          ic=1
+          ir=ir+1
+        goto 400
+ 410    write(*, *) "done simulating"
         return
       end
       program life
@@ -41,8 +101,7 @@ c       variable declaration
         integer ir, jr, ic, jc, rowc, colc
         integer file, ios, gen, row, col
         character pat1(1:100)*80, pat2(1:100)*80
-        logical eqsame
-        logical issame
+        logical eqsame,issame
         call getarg(1, arg)
         write(*,*) arg
         file=1
@@ -75,13 +134,16 @@ c       read all line of pattern
  200    ir = 1
  201    if ( ir.gt.row ) goto 220
           write(*,"(A)") pat1(ir)
-          pat2(ir)=pat1(ir)
+c          pat2(ir)=pat1(ir)
           ir=ir+1
         goto 201
  220    write(*, *) "end reading file"
+c       copy pattern
+        call copypa(pat1,pat2,row,col)
 c       start simulating
 c       compare 2 pattern
         eqsame = issame(pat1,pat2,row,col)
         if(eqsame) write(*,*) "2 pattern is same"
+        call sim(pat1,pat2,row,col)
         stop 'quit normally'
       end
